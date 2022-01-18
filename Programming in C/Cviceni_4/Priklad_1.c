60978c4013e65fc7507db13ff540a38073fb3ae5
mahonycz13 <mahonycz13@protonmail.com>
2022-01-18 21:00:53 +0100

    Finished Cviceni_4

--- Programming in C/Cviceni_4/Priklad_1.c
+++ Programming in C/Cviceni_4/Priklad_1.c
@@ -26,10 +26,47 @@
 
 #include<stdio.h>
 
+int PrintArray(int pole[], int length)
+{
+    int i;
+    //int length = sizeof(pole) / sizeof(pole[0]);
 
+    for(i = 0; i < length; i++)
+    {
+        printf("%d ,", pole[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int BetterBubbleSort(int pole[], int length)
+{
+    int i, j, temp;
+
+    for(i = 0; i < length; i++)
+    {
+        for(j = 0; j < length - i; j++)
+        {
+            if(pole[j] > pole[j+1])
+            {
+                temp = pole[j];
+                pole[j] = pole[j+1];
+                pole[j+1] = temp;
+            }
+        }
+    }
+    return 0;
+}
 
 int main()
 {
-    printf("Hello!\n");
+    int pole[] = { 1, 5, 3 ,6 , 90, 2 };
+    int length = sizeof(pole) / sizeof(pole[0]);
+    
+    PrintArray(pole, length);
+    BetterBubbleSort(pole, length);
+    PrintArray(pole, length);
+
+
     return 0;
 }
