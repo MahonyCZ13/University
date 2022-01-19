111706803ce275523da64c9c5626432d2bda1ef4
mahonycz13 <mahonycz13@protonmail.com>
2022-01-19 20:12:47 +0100

    prepared file for Cviceni_6

---
+++ Programming in C/Cviceni_6/Priklad_1.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  priklad_1.c
+ *
+ *    Description:  1. Napište funkci, která alokuje prostor pro matici celých čísel n*m.
+ *                  2. Napište funkce, které spočtou nejmenší společný násobek (nsn) a největší společný
+ *                  dělitel (nsd) dvou čísel.
+ *                  3. Napište funkci, která do každého prvku zadané matice uloží výsledek zadané funkci se
+ *                  dvěma parametry typu int vracející int. Parametry funkce jsou x a y a určují pozice
+ *                  prvku v matici. Použijte pointry na funkce.
+ *                  4. Napište program s parametry "{-nsn | -nsd } [-f output] n [m]", který spočte nsd nebo
+ *                  nsn pro všechny uspořádané dvojice čísel [x,y] takových, že x je z 1..n a y je z 1..m.
+ *                  Pokud m není zadáno, bude mít stejnou hodnotu jako n.
+ *                  5. Obsah matice bude vypsán na obrazovku       
+ *
+ *        Remarks:  Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+ *                  Neokopiroval jsem ani neopsal jsem cizi zdrojove kody
+ *                  Nikdo me pri vypracovani neradil
+ *                  Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+ *                  NENI MOJE TVORBA
+ *                  Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+ *                  Petr Maronek, 36456
+ *
+ *        Version:  0.1
+ *        Created:  15/01/2021 16:11:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Petr Maronek, 36456 
+ *   Organization:  VSFS
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+
+
+int main()
+{
+    
+    
+
+    return 0;
+}
