b5f2c6f6c5dad1cbe3f7208bf9af73f6635d8dfd
mahonycz13 <mahonycz13@protonmail.com>
2022-01-20 09:04:32 +0100

    added Read file function

--- Programming in C/Cviceni_7/Priklad_1.c
+++ Programming in C/Cviceni_7/Priklad_1.c
@@ -25,37 +25,86 @@
  */
 
 #include<stdio.h>
+#include<stdlib.h>
 
+// Test Function
+/*
 int DoIt(char* first, char* second, int one, int two)
-{
-    printf("%s and %s | %d and %d", first, second, one, two);
+ {
+    printf("%s and %s | %d and %d\n", first, second, one, two);
     return 0;
+} 
+*/
+void ErrorHandler(int ex)
+{
+    switch(ex)
+    {
+        case 10: printf("Parametr -i je povinny. Zadejte cestu k textovemu souboru, ktery chcete zpracovat.\n");
+        case 11: printf("Chyba pri otevirani souboru. Pouzijte parametry kompilatoru pro vetsi podrobnosti.\n");
+        default: printf("Genericka chyba. Vyuzijte vystupu na konzoli.\n");
+    }
+    
+    //return 0;
 }
 
+void ProcessFile(char* path)
+{
+    FILE* myFile;
+    int c;
+    char* fileContent;
+    long length;
+    char c1;
+    int i = 0;
+
+    if( !(myFile = fopen( path, "rw"))) ErrorHandler(11);
+    /*
+    while((c = getc( myFile)) != EOF) fileContent = putchar(c);
+    
+    //fclose(myFile);
+    if(myFile)
+    {
+        
+        fseek(myFile, 0 , SEEK_END);
+        length = ftell(myFile);
+        //fseek(myFile, 0, SEEK_SET);
+        fileContent = malloc(length);
+
+        if(fileContent)
+        {
+            fread(fileContent, 1, length, myFile);
+        }
+        fclose(myFile);
+
+    }*/
+    
+    while((c1 = fgetc(myFile)) != EOF)
+    {
+        fileContent[i] = c1;
+        printf("%c", c1);
+        i = i+1;
+    }
+
+    //printf("%s", fileContent);
+
+}
 int main(int argc, char** argv)
 {
-    int w = 0;
-    int o = 0;
+    char* path;
+
     while(*++argv && **argv=='-')
     {
         switch(argv[0][1])
         {
-            case 'w': w = 1; break;
-            case 'o': o = 1; break;
-            default: error();
+            case 'i': path = argv[1]; break;
+            default: ErrorHandler(10);
         }
         
     }
-    DoIt(argv[0], argv[1], w, o);
-    // File accessing
-    /* FILE* myFile;
-    int c;
-
-    if( !(myFile = fopen("test.txt", "r"))) error();
     
-    while((c = getc( myFile)) != EOF) putchar(c);
+    if(path == NULL) return 0;
+    else ProcessFile(path);
+    //DoIt(argv[0], argv[1], w, o);
     
-    fclose(myFile); */
     
     return 0;
 }
