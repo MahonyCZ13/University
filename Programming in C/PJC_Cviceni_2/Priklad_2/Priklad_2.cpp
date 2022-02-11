2848acb7305951d5aef6bf44e36460ff7fd0969b
mahonycz13 <mahonycz13@protonmail.com>
2022-02-11 12:10:50 +0100

    Transfer solution to VS

---
+++ Programming in C/PJC_Cviceni_2/Priklad_2/Priklad_2.cpp
@@ -0,0 +1,83 @@
+﻿/*
+ * =====================================================================================
+ *
+ *       Filename:  priklad_2.c
+ *
+ *    Description:  Napište program, který pro daný měsíc (stačí jen do září) v roce zadaný číslem vypíše jeho název.
+ *
+ *        Remarks:  Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+ *                  Neokopiroval jsem ani neopsal jsem cizi zdrojove kody
+ *                  Nikdo me pri vypracovani neradil
+ *                  Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+ *                  NENI MOJE TVORBA
+ *                  Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+ *                  Petr Maronek, 36456
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2021 09:45:00 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Petr Maronek, 36456
+ *   Organization:  VSFS
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    printf("Zadejte mesic (1-12):\n");
+
+    // Inicializace promenne
+    int mesic;
+
+    // Precteme od uzivatele vstup a ulozime do promenne 'mesic' pomoci pointeru
+    scanf_s("%d", &mesic);
+
+    // Pomoci SWITCH funkce rozpozname mesic
+    switch (mesic)
+    {
+    case 1:
+        printf("Leden.\n");
+        break;
+    case 2:
+        printf("Unor\n");
+        break;
+    case 3:
+        printf("Brezen\n");
+        break;
+    case 4:
+        printf("Duben\n");
+        break;
+    case 5:
+        printf("Kveten\n");
+        break;
+    case 6:
+        printf("Cerven\n");
+        break;
+    case 7:
+        printf("Cervenec\n");
+        break;
+    case 8:
+        printf("Srpen\n");
+        break;
+    case 9:
+        printf("Zari\n");
+        break;
+    case 10:
+        printf("Rijen\n");
+        break;
+    case 11:
+        printf("Listopad\n");
+        break;
+    case 12:
+        printf("Prosinec\n");
+        break;
+    default:
+        printf("Tento mesic neznam :'(\n");
+        break;
+    }
+
+    return 0;
+}
---
+++ Programming in C/PJC_Cviceni_2/Priklad_4/Priklad_4.cpp
@@ -0,0 +1,41 @@
+﻿/*
+ * =====================================================================================
+ *
+ *       Filename:  priklad_4.c
+ *
+ *    Description:  Napište program, který vygeneruje všechny textové řetězce délky X=3 z písmen T G C A.
+ *
+ *        Remarks:  Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+ *                  Neokopiroval jsem ani neopsal jsem cizi zdrojove kody
+ *                  Nikdo me pri vypracovani neradil
+ *                  Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+ *                  NENI MOJE TVORBA
+ *                  Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+ *                  Petr Maronek, 36456
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2021 10:33:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Petr Maronek, 36456
+ *   Organization:  VSFS
+ *
+ * =====================================================================================
+ */
+
+int main()
+{
+    int i;
+    int x = 3;
+    char t = 'T';
+    char g = 'G';
+    char c = 'C';
+    char a = 'A';
+
+    for (i = 0; i <= 4; i++)
+    {
+
+    }
+    return 0;
+}
