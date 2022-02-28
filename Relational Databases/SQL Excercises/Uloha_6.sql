786549134249e4327176b46c907aa9233a2eb835
mahonycz13 <mahonycz13@protonmail.com>
2022-02-28 16:16:22 +0100

    Uloha 6 done

--- Relational Databases/SQL Excercises/Uloha_6.sql
+++ Relational Databases/SQL Excercises/Uloha_6.sql
@@ -1,8 +1,2 @@
-SELECT Predmet.nazev 
-FROM Predmet
-JOIN 
-    Zapis ON(Zapis.kod = Predmet.kod) 
-JOIN 
-    Student ON(Student.rc = Zapis.rc) 
-WHERE 
-    Student.jmeno = 'Fejfar'
+SELECT DISTINCT Predmet.nazev FROM Predmet JOIN Zapis ON(Zapis.kod = Predmet.kod) JOIN Student ON(Student.rc = Zapis.rc)
+WHERE Student.jmeno = 'Jan Fejfar'
