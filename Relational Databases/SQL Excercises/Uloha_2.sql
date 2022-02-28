3dad6454950fd283a52f7fad4d0a6a19f661f43b
mahonycz13 <mahonycz13@protonmail.com>
2022-02-28 15:38:47 +0100

    formatting

--- Relational Databases/SQL Excercises/Uloha_2.sql
+++ Relational Databases/SQL Excercises/Uloha_2.sql
@@ -1,2 +1,5 @@
-SELECT Predmet.nazev, Predmet.garant, Zapis.semestr FROM Zapis JOIN Predmet ON(Zapis.kod = Predmet.kod)
+SELECT Predmet.nazev, Predmet.garant, Zapis.semestr 
+FROM Zapis 
+JOIN 
+    Predmet ON(Zapis.kod = Predmet.kod)
 WHERE Zapis.semestr = '2010L' AND Predmet.garant <> 'Blaha'
