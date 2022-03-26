c632f92cc48ca59762e505aa0cdd46a7d527ce1d
mahonycz13 <mahonycz13@protonmail.com>
2022-03-26 21:25:36 +0100

    pridana klauzule na Pohled

--- Relational Databases/Views/Ukol_2.sql
+++ Relational Databases/Views/Ukol_2.sql
@@ -1,4 +1,5 @@
 -- Pohled neni aktualizovatelny
+CREATE VIEW PrehledObjednavek AS
 SELECT ZBOZ.KAT AS 'Kategorie', SUM(POLOZ.VCEN) AS 'Celokova Cena', SUM(POLOZ.MNOZ) AS 'Mnozstvi' 
 FROM POLOZ 
 JOIN ZBOZ ON(POLOZ.KOD = ZBOZ.KOD) 
