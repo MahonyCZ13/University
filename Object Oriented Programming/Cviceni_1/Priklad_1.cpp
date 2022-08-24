5c315fca0ea9f85bb400088bf83476435a04bc85
mahonycz13 <mahonycz13@protonmail.com>
2022-08-24 22:37:24 +0200

    Started debugging

--- Object Oriented Programming/Cviceni_1/Priklad_1.cpp
+++ Object Oriented Programming/Cviceni_1/Priklad_1.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 int main()
 {
-    //Zvire pytlik; // Bezparametrova
-    //Zvire moucha(20); // Parametrova
-    //Zvire beruska(pytlik); // Copy konstruktor
+    Zvire pytlik; // Bezparametrova
+    Zvire moucha(20); // Parametrova
+    Zvire beruska(pytlik); // Copy konstruktor
 
-    //pytlik.jez(5);
-    //pytlik.vymesuj(3);
+    pytlik.jez(5);
+    pytlik.vymesuj(3);
 
-    //if (!pytlik.zije()) return -1;
+    if (!pytlik.zije()) return -1;
 
-    //pytlik.vymesuj(4);
+    pytlik.vymesuj(4);
 
-    //if (!pytlik.jez(1)) return -2;
+    if (!pytlik.jez(1)) return -2;
 
-    Student Petr;
+    /* Student Petr;
 
     int hotovo = Petr.hodnoceni(4);
 
     if (hotovo == 1) printf("Student splnil se znamkou 2");
-    else printf("Student predmet nesplnil");
+    else  */printf("Student predmet nesplnil");
 
     return 0;
 }
