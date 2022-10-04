87e21dc782c93c7827e3043162618919df744928
mahonycz13 <mahonycz13@protonmail.com>
2022-10-04 22:26:43 +0200

    started C++ learning

---
+++ CPPin21Days/week1/day1/cviceni1.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cviceni1.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/04/2022 10:13:39 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  MahonyCZ13 (), mahonycz13@protonmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main()
+{
+    int x = 5;
+    int y = 7;
+    std::cout << x + y << " " << x * y;
+    return 0; 
+}
---
+++ CPPin21Days/week1/day1/nazdar.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  nazdar.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/04/2022 10:02:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  MahonyCZ13 (), mahonycz13@protonmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Nazdar svete!\n";
+    return 0;
+}
